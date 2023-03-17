c85980d7faad791ff2a16edd9682d6469af20c23
liuhuan1004 <liuhuan1004@163.com>
2023-03-18 00:05:21 +0800

    add sub program

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include "stdio.h"
-
+#include "sub.h"
 
 int main(void)
 {
 
     printf("hello world.this is the first git\n");
-    printf("I'll go to the playground.\n");
+    printf("10+20 = %d.\n",sum(10,20));
     return 0;
 }
