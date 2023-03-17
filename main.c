96b6ad000d0a4f379b8d1ecfd2ee55e085293304
liuhuan1004 <liuhuan1004@163.com>
2023-03-18 07:40:48 +0800

    add sub function .

--- main.c
+++ main.c
@@ -6,5 +6,7 @@ int main(void)
 
     printf("hello world.this is the first git\n");
     printf("10+20 = %d.\n",sum(10,20));
+    printf("20-10 = %d.\n",sub(20,10));
+
     return 0;
 }
--- sub.c
+++ sub.c
@@ -2,3 +2,8 @@ int sum(int a, int b)
 {
     return a+b;
 }
+
+int sub(int a , int b)
+{
+    return a-b;
+}
